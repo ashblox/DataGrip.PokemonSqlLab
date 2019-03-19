d90e0d4870b318a7e47b2e45eb97332fe3b4d757
Ashley Bloxom <47283036+ashblox@users.noreply.github.com>
2019-03-19 16:54:46 -0400

    incorporated wilhem's changes

--- part4.sql
+++ part4.sql
@@ -3,9 +3,9 @@
     # I thought the best way to order the trainers would be take an average of the totals of these fields
     # for each of their pokemon. The "best" trainer will have pokemon with the highest average in these fields.
 
-SELECT GROUP_CONCAT(p.name) as "Pokemon", GROUP_CONCAT(tr.trainername) as "Trainer", 
-       GROUP_CONCAT(pt.pokelevel) as "PokeLevels", GROUP_CONCAT(t.name) as "Primary Types", 
-       GROUP_CONCAT(r.name) as "Secondary Types"
+SELECT DISTINCT GROUP_CONCAT(p.name) as "Pokemon", tr.trainername as "Trainer",
+       GROUP_CONCAT(pt.pokelevel) as "PokeLevels", GROUP_CONCAT(t.name) as "Primary Types",
+       GROUP_CONCAT(r.name) as "Secondary Types", AVG_POINTS.total
 FROM pokemon_trainer pt
 JOIN (
          SELECT trainerID, AVG(hp + maxhp + attack + defense + spatk + spdef + speed) as total
@@ -16,5 +16,6 @@ JOIN pokemons p ON p.id = pt.pokemon_id
 JOIN trainers tr ON tr.trainerID = pt.trainerID
 JOIN types t ON t.id = p.primary_type
 JOIN types r ON r.id = p.secondary_type
-GROUP BY AVG_POINTS.total
-ORDER BY AVG_POINTS.total DESC;
+GROUP BY AVG_POINTS.total, tr.trainerID, tr.trainername
+ORDER BY AVG_POINTS.total DESC
+LIMIT 20;
