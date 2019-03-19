4a53a8038dfab8fd02295f4ed67e1214a7fb03b6
Ashley Bloxom <47283036+ashblox@users.noreply.github.com>
2019-03-18 22:27:56 -0400

    Create part4.sql

---
+++ part4.sql
@@ -0,0 +1,21 @@
+# Part 4: Final Report
+# Explanation: Since pokelevel directly influences the fields HP, max HP, attack, defense, etc., 
+    # I thought the best way to order the trainers would be take an average of the totals of these fields
+    # for each of their pokemon. The "best" trainer will have pokemon with the highest average in these fields.
+    # I have included the first 20 entries in a .tsv file but thought it best to omit the rest.
+
+SELECT GROUP_CONCAT(p.name) as "Pokemon", GROUP_CONCAT(tr.trainername) as "Trainer", 
+       GROUP_CONCAT(pt.pokelevel) as "PokeLevels", GROUP_CONCAT(t.name) as "Primary Types", 
+       GROUP_CONCAT(r.name) as "Secondary Types"
+FROM pokemon_trainer pt
+JOIN (
+         SELECT trainerID, AVG(hp + maxhp + attack + defense + spatk + spdef + speed) as total
+         FROM pokemon_trainer
+         GROUP BY trainerID
+) AS AVG_POINTS ON AVG_POINTS.trainerID = pt.trainerID
+JOIN pokemons p ON p.id = pt.pokemon_id
+JOIN trainers tr ON tr.trainerID = pt.trainerID
+JOIN types t ON t.id = p.primary_type
+JOIN types r ON r.id = p.secondary_type
+GROUP BY AVG_POINTS.total
+ORDER BY AVG_POINTS.total DESC;
